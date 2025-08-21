4706722722c85215d797ccc9f39f9e005d9a3dbc
Rachata Singkhet <161452629+Kyaputan@users.noreply.github.com>
2025-08-21 11:39:58 +0700

    Update

--- Backend/app/db/base.py
+++ Backend/app/db/base.py
@@ -1,4 +1,4 @@
 from sqlalchemy.orm import declarative_base
+from app.models.vehicle import Vehicle
 
 Base = declarative_base()
-from app.models.vehicle import Vehicle
--- Backend/app/schemas/license.py
+++ Backend/app/schemas/license.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel, Field
 class InsertPlate(BaseModel):
     license: str = Field(..., min_length=1)
     province: str = Field(..., min_length=1)
-    allowed: bool = False  
+    allowed: bool  
 
 class PlateOut(BaseModel):
     license: str
     province: str
-    allowed: bool
+    allowed: bool = True
